42331da6a80958b110251dea39712d71f8430737
Lucas Meneses <lucasfelix3194@gmail.com>
2021-09-02 19:47:13 -0300

    Algoritmo de busca (Binaria) em Python.

---
+++ Algoritmos de busca/buscaBinaria.py
@@ -0,0 +1,24 @@
+def buscaBinaria(lista: list, el: float or int):
+    """
+    Se espera que a lista já esteja ordenada, então busca se o elemento está ou
+    não na lista.
+    :param lista: lista de numeros ordenados
+    :param el: Elemento da busca
+    :return: True se existe / False se não existe
+    """
+    if len(lista) == 1:
+        return lista[0] == el
+
+    meio = len(lista) // 2
+
+    if lista[meio] == el:
+        return True
+    elif lista[meio] > el:
+        return buscaBinaria(lista[:meio], el)
+    else:
+        return buscaBinaria(lista[meio:], el)
+
+
+lista = [1, 2, 3, 4, 5]
+print(buscaBinaria(lista, 2))
+print(buscaBinaria(lista, 6))
