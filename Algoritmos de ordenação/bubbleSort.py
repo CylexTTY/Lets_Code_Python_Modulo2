52fe66bbc38b0f99cb97ef2315b55a2697eaf548
Lucas Meneses <lucasfelix3194@gmail.com>
2021-09-02 11:09:17 -0300

    Algoritmo de ordenação (Bubble Sort) em Python.

--- Algoritmos de ordenação/bubbleSort.py
+++ Algoritmos de ordenação/bubbleSort.py
@@ -1,15 +1,26 @@
 import random
 
-
-def bubbleSort(lista):
+# Opção de fazer por ordem crescente ou decrescente.
+def bubbleSort(lista, reverse=False):
+    '''
+    Organiza uma lista de forma crescente ou decrescente usando o algoritmo de
+    Bubble Sort.
+    :param lista: lista principal
+    :param reverse: False (se ordem crescente) / True (se ordem decrescente)
+    :return: lista ordenada
+    '''
     for i in range(len(lista) - 1):
-        for j in range(len(lista) - i -1):
-            if lista[j] > lista[j+1]:
-                lista[j], lista[j+1] = lista[j+1], lista[j]
+        for j in range(len(lista) - i - 1):
+            if not reverse:
+                if lista[j] > lista[j+1]:
+                    lista[j], lista[j+1] = lista[j+1], lista[j]
+            elif lista[j] < lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
     return lista
 
 
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 random.shuffle(lista)
-print('Shuffle list:', lista)
-print('Ordered list:', bubbleSort(lista))
+print('Shuffle list:    ', lista)
+print('Ascending Order: ', bubbleSort(lista))
+print('Descending Order:', bubbleSort(lista, reverse=True))
